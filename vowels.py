b519d726b130dbae1b830d06f6885fb6bcdd5a71
Anandhakumar12 <51222581+Anandhakumar12@users.noreply.github.com>
2019-07-12 16:09:48 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,4 +1,4 @@
-z=input("enter the character")
+z=input()
 if(z=='A' or z=='E' or z=='I' or z=='O' or z=='U' or z=='a' or z=='e' or z=='i' or z=='o' or z=='u'):
   print("Vowel")
 elif(z.isalpha())==False):
