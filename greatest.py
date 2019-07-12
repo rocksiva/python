cfddca6bd9f5196e8299218a9d7134621b260988
Anandhakumar12 <51222581+Anandhakumar12@users.noreply.github.com>
2019-07-12 16:46:36 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a>b and a>c):
+  print(a)
+elif(b>a and b>c):
+  print(b)
+elif(c>a and c>b):
+  print(c)
+elif(a=b):
+  print(a)
+elif(b=c):
+  print(b)
+elif(a=c):
+  print(a)
+elif(a=b=c):
+  print(a)
