9ff99d0faee16ad6a50fd606c30298849f2f3354
Anandhakumar12 <51222581+Anandhakumar12@users.noreply.github.com>
2019-07-12 16:05:05 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+z=input("enter the character")
+if(z=='A' or z=='E' or z=='I' or z=='O' or z=='U' or z=='a' or z=='e' or z=='i' or z=='o' or z=='u'):
+  print("Vowel")
+elif(z.isalpha())==False):
+  print("Invalid")
+else:
+  print("Consonant")
